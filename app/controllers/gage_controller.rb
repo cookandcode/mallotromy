e0eae0bcea59962069f14d078a243ccb120e6ded
Georges <georges@MacBook-Pro-de-Georges-2.local>
2013-01-17 20:04:12 +0100

    static pages37

--- app/controllers/gage_controller.rb
+++ app/controllers/gage_controller.rb
@@ -1,5 +1,8 @@
 class GageController < ApplicationController
 	def index
+		@title_page = "Mallotromy Application Jeu Alcool et Gages - Lille France"
+		@meta_description = "Mallotromy est une application pour toutes vos soirées entre amis. Gages, jeux, alcool,"
+		@meta_keyword = "Gages, Alcool, Jeu, Application, Lille, France, Mallotromy"
 		@gages = Gage.all
 
 		respond_to do |wants|
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,13 @@
 class StaticController < ApplicationController
   def about
+  	@title_page = "A propos de  Mallotromy - Lille France"
+	@meta_description = "Explication sur Mallotromy. Mallotromy est un jeu de gage et un jeu d'alcool pour se divertir en soirée "
+	@meta_keyword = "Gages, Alcool, Jeu, Application, Lille, France, Mallotromy, gallery, about, a propos, soirée, mariages"
   end
 
   def rules
+  	@title_page = "Les règles de Mallotromy - Lille France"
+	@meta_description = "Les règles du jeu Mallotromy. Jouer à malltromy durant vos soirée, mariages, enterrement de vie de garçon et enterrement de vie de jeune fille "
+	@meta_keyword = "Gages, Alcool, Jeu, Application, Lille, France, Mallotromy, règles, rules, soirée, mariages"
   end
 end
