7f1061108d9cb869cbe625946e3a0e1e361b1732
Georges <georges@MacBook-Pro-de-Georges-2.local>
2013-01-26 18:50:20 +0100

    landing in index page

--- config/routes.rb
+++ config/routes.rb
@@ -65,7 +65,7 @@ Mallotromy::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'gage#index'
+  root :to => 'gage#landing'
 
   # See how all your routes lay out with "rake routes"
 
